ff934af7c0f775ec0cd7adf569b5e441e1bc9a4d
Dave Smith <dasmithm@amazon.com>
2018-07-12 10:48:51 -0400

    Colorize author names
    This is an overly complicated approach, but it seems to give pretty
    diverse color results.

--- website/static/chat.js
+++ website/static/chat.js
@@ -12,7 +12,7 @@ function refreshChatMessages() {
           html += ("<div>" +
             "<button class=\"x-button\" data-id=\"" + message.id + "\">Delete</button> " +
             "<i>" + date.toLocaleTimeString() + "</i> " +
-            "<b>" + escape(message.author_name) + "</b>: " +
+            "<b style=\"color: " + authorToColor(message.author_name) + "\">" + escape(message.author_name) + "</b>: " +
             escape(message.text) + "</div>")
         })
 
@@ -38,6 +38,81 @@ function refreshChatMessages() {
             })
         })
     })
+}
+
+function authorToColor(authorName) {
+  var r = 0, g = 0, b = 0
+  var hash = hashCode(authorName)
+
+  // For each digit in the hash, add it to the r, g, or b bucket (with some weird multipliers to increase diversity)
+  var digitCount = 0
+  var toProcess = hash
+  while (toProcess > 0) {
+    digitCount++
+    var temp = toProcess % 10  * (3 ** digitCount)
+    toProcess /= 10
+    toProcess |= 0
+    switch (digitCount % 3) {
+      case 0:
+        r += temp
+        break;
+      case 1:
+        g += temp
+        break;
+      case 2:
+        b += temp
+    }
+  }
+
+  // Make sure each component is between 0 and 255
+  r %= 256
+  g %= 256
+  b %= 256
+
+  // If the color is too light, darken its strongest component
+  var luminance = 0.299*r**2 + 0.587*g**2 + 0.114*b**2
+  if (luminance > 30000) {
+    if (r > g && r > b) {
+      r /= 2
+    }
+    else if (g > r && g > b) {
+      g /= 2
+    }
+    else {
+      b /= 2
+    }
+  }
+
+  // If the color is too dark, brighten one of the components:
+  if (luminance < 5000) {
+    if (r < g && r < b) {
+      r = Math.max(100, r * 2)
+    }
+    else if (g < r && g < b) {
+      g = Math.max(100, g * 2)
+    }
+    else {
+      b = Math.max(100, b * 2)
+    }
+  }
+
+  r |= 0
+  g |= 0
+  b |= 0
+
+  return "rgb(" + r + ", " + g + ", " + b + ")"
+}
+
+function hashCode(string) {
+  // Stolen from: https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
+  var hash = 0
+  if (string.length === 0) return hash;
+  for (var i = 0; i < string.length; i++) {
+    var chr   = string.charCodeAt(i)
+    hash  = ((hash << 5) - hash) + chr
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
 }
 
 function postMessage(authorName, messageText) {
